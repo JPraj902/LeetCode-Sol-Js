4c4a3a1a5dd1a2d293796c417296a739ab814d01
Jugal Prajapati <jpwhatsapp9146@gmail.com>
2024-12-31 23:41:34 +0530

    Time: 0 ms (100%), Space: 49 MB (64.65%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1, m, nums2, n) {
+    let p = m + n - 1, p1 = m - 1, p2 = n - 1;
+
+    while (p1 >= 0 && p2 >= 0) nums1[p--] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
+    while (p2 >= 0) nums1[p--] = nums2[p2--];
+}
